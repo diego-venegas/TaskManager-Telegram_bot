f83b190deec744ff63149cc5171a9ed55b6cc6e6
Diego Valdés <diego_alejo7@hotmail.com>
2021-11-30 18:51:22 -0300

    Funcionalidades base de tasksController.py

---
+++ tasksController.py
@@ -0,0 +1,25 @@
+list_tasks = []
+
+
+def sort_tarea():
+    return None
+
+
+def add_task():
+    return None
+
+
+def remove_task():
+    return None
+
+
+def modify_task():
+    return None
+
+
+def get_task():
+    return None
+
+
+def print_task():
+    return None
