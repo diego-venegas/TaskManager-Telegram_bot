519f80b585938959ef4e22a5d65fa5ef4975faa5
Orlando Cavieres <frizortake@gmail.com>
2021-12-02 00:10:09 -0300

    Se agrega control de flujo y respuestas a modificaciones

--- tasksController.py
+++ tasksController.py
@@ -52,7 +52,17 @@ def modify_task(title, new_title, new_description, new_date):
     
     task_to_modify = get_task(title)
     
-    return None
+    if task_to_modify is not None:
+        
+        task_to_modify.modify_title(new_title)
+        task_to_modify.modify_description(new_description)
+        task_to_modify.modify_date_time(new_date)
+        print(f"Tarea Modificada")
+        return True
+        
+    else:
+        print(f"Error, {title} no se encuentra creada")
+        return False
 
 
 def get_task(title):
