fa0c640f4e2dc41ba7b24fb74c7474ba635221cf
Orlando Cavieres <frizortake@gmail.com>
2021-12-13 00:33:33 -0300

    Establecer todas las funciones del bot en un punto central y empleando botones en vez de comandos

--- botController.py
+++ botController.py
@@ -1,14 +1,17 @@
 import logging
 from datetime import datetime, timedelta
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Updater,
     CommandHandler,
     MessageHandler,
     Filters,
-    ConversationHandler
+    ConversationHandler,
+    CallbackQueryHandler, CallbackContext
 )
 
+from taskClass import ClassTask
 from tasksController import *
 
 # Logging
@@ -22,14 +25,9 @@ description = ''
 date_user = ''
 chat_id = 0
 
-# Pasos crear tarea
-CT_TITLE, CT_DESCRIPTION, CT_DATE = range(3)
-
-# Pasos eliminar tarea
-DE_NAME = range(1)
-
-# Pasos editar tarea
-ED_FIND, ED_TITLE, ED_DESCRIPTION, ED_DATE = range(4)
+# Etapas
+INIT, CREATE_TITLE, CREATE_DESCRIPTION, CREATE_DATE, DELETE, EDIT_SEARCH_TITLE, \
+    EDIT_TITLE, EDIT_DESCRIPTION, EDIT_DATE = range(9)
 
 
 # FUNCIONES DE VERIFICACION Y CHEQUEO #############################################
@@ -68,20 +66,23 @@ def verify_date(text):
     return ''
 
 
-##########################################
+# FLUJO PARA CREAR TAREA ##########################################################
 
-def create_steps(update, context):
+def create_steps(update: Update, context: CallbackContext) -> int:
     global title, description, date_user
     title = ''
     description = ''
     date_user = ''
 
-    update.message.reply_text(f"Ingresa el nombre de la tarea a crear. Esta debe tener 4 o más caracteres"
-                              f"\n>Si quieres cancelar el proceso, presiona este comando /Cancel")
-    return CT_TITLE
+    query = update.callback_query
+    query.answer()
+
+    query.edit_message_text(text = f"Ingresa el nombre de la tarea a crear. Esta debe tener 4 o más caracteres\n"
+                                   f">Si quieres cancelar el proceso, presiona este comando /Cancel")
+    return CREATE_TITLE
 
 
-def create_step_title(update, context):
+def create_step_title(update: Update, context: CallbackContext) -> int or None:
     global title
     title = update.message.text
 
@@ -98,14 +99,14 @@ def create_step_title(update, context):
 
         update.message.reply_text(f"Ingresa la descripción de la tarea a crear. Esta debe tener 4 o más caracteres"
                                   f"\n>Si quieres cancelar el proceso, presiona este comando /Cancel")
-        return CT_DESCRIPTION
+        return CREATE_DESCRIPTION
 
 
-def create_step_description(update, context):
+def create_step_description(update: Update, context: CallbackContext) -> int or None:
     global description
     description = update.message.text
 
-    if description == '/Cancel':
+    if description == '/Cancel' or description == '/cancel':
         update.message.reply_text(f"Has cancelado la acción!!")
         return ConversationHandler.END
 
@@ -116,11 +117,12 @@ def create_step_description(update, context):
             update.message.reply_text(verification)
             return None
 
-        update.message.reply_text(f"Ingresa la fecha de la tarea a crear, \nel formato debe ser en DD/MM/YYYY hh:mm")
-        return CT_DATE
+        update.message.reply_text(f"Ingresa la fecha de la tarea a crear,\nel formato debe ser en DD/MM/YYYY hh:mm"
+                                  f"\n>Si quieres cancelar el proceso, presiona este comando /Cancel")
+        return CREATE_DATE
 
 
-def create_step_date(update, context):
+def create_step_date(update: Update, context: CallbackContext) -> int or None:
     global title, description, date_user
     date_user = update.message.text
 
@@ -137,34 +139,47 @@ def create_step_date(update, context):
 
         add_task(ClassTask(title, description, date_user))
 
-        update.message.reply_text(f"La tarea ha sido creada con éxito")
+        update.message.reply_text(f"La tarea ha sido creada con éxito.\n"
+                                  f"Si desea realizar otra tarea, presione este comando: /Opciones")
         return ConversationHandler.END
 
 
-############################################
+# PASO PARA VISUALIZAR LISTA DE TAREAS ###########################################
 
+def visualize(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
 
-def visualize(update, context):
     text = visualize_tasks()
 
     if len(text) > 0:
-        update.message.reply_text(text)
-        start(update, context)
+        query.edit_message_text(text = text)
 
     else:
-        update.message.reply_text("No hay tareas agregadas. ¡Empieza a crear!")
+        query.edit_message_text("No hay tareas agregadas en la lista. ¡Empieza a crear tareas!\n"
+                                "Presiona este comando: /Opciones y selecciona la opción Crear")
+
+    return ConversationHandler.END
 
 
-############################################
+# PASOS PARA EDITAR TAREA ########################################################
+
+def edit_steps(update: Update, context: CallbackContext) -> int or None:
+    query = update.callback_query
+    query.answer()
+    text = visualize_tasks()
+
+    if len(text) > 0:
+        query.edit_message_text(text = f"Ingresa el nombre de la tarea a editar. Esta debe tener 4 o más caracteres\n"
+                                       f">Si quieres cancelar el proceso, presiona este comando /Cancel")
+        return EDIT_SEARCH_TITLE
+
+    else:
+        query.edit_message_text("No hay tareas en la lista. No se puede editar. Se sugiere crear una tarea.\n"
+                                "Para ello presione este comando /Opciones y elija la opción Crear.")
 
 
-def edit_steps(update, context):
-    update.message.reply_text(f"Ingresa el nombre de la tarea a editar. Esta debe tener 4 o más caracteres"
-                              f"\n>Si quieres cancelar el proceso, presiona este comando /Cancel")
-    return ED_FIND
-
-
-def edit_step_find(update, context):
+def edit_step_find(update: Update, context: CallbackContext) -> int or None:
     global title, description, date_user
     title = update.message.text
 
@@ -177,8 +192,8 @@ def edit_step_find(update, context):
 
         if isinstance(task, type(None)):
             update.message.reply_text(
-                f"Tarea no encontrada. ¡Reintentalo de nuevo!"
-                f"\n>Si quieres cancelar el proceso, presiona este comando /Cancel")
+                    f"Tarea no encontrada. ¡Reintentalo de nuevo!"
+                    f"\n>Si quieres cancelar el proceso, presiona este comando /Cancel")
             return None
 
         else:
@@ -188,10 +203,10 @@ def edit_step_find(update, context):
             update.message.reply_text(f"El nombre actual de la tarea es '{title}'.\n"
                                       f"Si deseas cambiar el nombre, escribe el nuevo, sino, escribe no.\n"
                                       f">Si quieres cancelar el proceso, presiona este comando /Cancel")
-            return ED_TITLE
+            return EDIT_TITLE
 
 
-def edit_step_change_title(update, context):
+def edit_step_change_title(update: Update, context: CallbackContext) -> int or None:
     global title, new_title, description
     new_title = update.message.text
 
@@ -200,16 +215,11 @@ def edit_step_change_title(update, context):
         return ConversationHandler.END
 
     else:
-
         if new_title == 'No' or new_title == 'no':
-
-            update.message.reply_text(f"El título de la tarea se mantiene como '{title}'\n"
-                                      f">Si quieres cancelar el proceso, presiona este comando /Cancel")
-
+            update.message.reply_text(f"El título de la tarea se mantiene como '{title}'")
             new_title = title
 
         else:
-
             verification = verify_title(new_title)
 
             if verification != '':
@@ -217,21 +227,19 @@ def edit_step_change_title(update, context):
                 return None
 
             if new_title == title:
-
                 update.message.reply_text(f"WARNING: El nuevo título de tarea es igual al anterior, "
                                           f"por lo que se mantiene '{title}'.\n")
 
             else:
-
-                update.message.reply_text(f"Se ha editado el título de la tarea con éxito a '{new_title}'.")
+                update.message.reply_text(f"Se editará el título de la tarea al finalizar el proceso a '{new_title}'.")
 
         update.message.reply_text(f"La descripción actual de la tarea es:\n{description}.\n"
                                   f"Si deseas cambiarla, escribe una nueva, sino, escribe 'no'.\n"
                                   f">Si quieres cancelar el proceso, presiona este comando /Cancel")
-        return ED_DESCRIPTION
+        return EDIT_DESCRIPTION
 
 
-def edit_step_change_description(update, context):
+def edit_step_change_description(update: Update, context: CallbackContext) -> int or None:
     global description, date_user
     new_description = update.message.text
 
@@ -253,18 +261,17 @@ def edit_step_change_description(update, context):
             if new_description == description:
                 update.message.reply_text(f"La descripción ingresada es igual a la anterior, por lo que se "
                                           f"mantiene:\n'{description}'\n")
-
             else:
                 description = new_description
-                update.message.reply_text(f"Se ha editado la descripción de la tarea con éxito a:\n'{description}'\n")
+                update.message.reply_text(f"Se editará la descripción de la tarea al finalizar el proceso a:\n'{description}'\n")
 
         update.message.reply_text(f"La fecha/hora actual de la tarea es '{date_user}'\n"
                                   f"Si deseas cambiarla, escribe una nueva, sino, escribe 'no'.\n"
                                   f">Si quieres cancelar el proceso, presiona este comando /Cancel")
-        return ED_DATE
+        return EDIT_DATE
 
 
-def edit_step_change_date(update, context):
+def edit_step_change_date(update: Update, context: CallbackContext) -> int or None:
     global title, new_title, description, date_user
     new_date = update.message.text
 
@@ -286,37 +293,46 @@ def edit_step_change_date(update, context):
             if new_date == date_user:
                 update.message.reply_text(f"La fecha/hora ingresada es igual a la anterior, por lo que se "
                                           f"mantiene: '{date_user}'\n")
-
             else:
                 date_user = new_date
-                update.message.reply_text(f"Se ha editado la fecha/hora de la tarea con éxito a '{date_user}'\n")
+                update.message.reply_text(f"Se editará la fecha/hora de la tarea al finalizar el proceso a '{date_user}'\n")
 
         modify_task(title, new_title, description, date_user)
-        update.message.reply_text(f"Se ha modificado con éxito la tarea.")
+        update.message.reply_text(f"Se ha modificado con éxito la tarea.\n"
+                                  f"Si desea realizar otra tarea, presione este comando: /Opciones")
         return ConversationHandler.END
 
 
-############################################
+# PASOS PARA ELIMINAR TAREA ########################################################
 
-
-def remove_steps(update, context):
+def remove_steps(update: Update, context: CallbackContext) -> int:
     global title
     title = ''
 
-    update.message.reply_text(f"Ingresa el nombre de la tarea que deseas eliminar, esta debe ser mayor a 4 caracteres"
-                              f"\n>Si quieres cancelar el proceso, presiona este comando /Cancel")
-    return DE_NAME
+    query = update.callback_query
+    query.answer()
+    text = visualize_tasks()
+
+    if len(text) > 0:
+        query.edit_message_text(text = f"Ingresa el nombre de la tarea que deseas eliminar. Esta debe tener 4 o más caracteres\n"
+                                       f">Si quieres cancelar el proceso, presiona este comando /Cancel")
+        return DELETE
+
+    else:
+        query.edit_message_text("No hay tareas en la lista. No se puede eliminar. Se sugiere crear una tarea.\n"
+                                "Para ello presione este comando: /Opciones y elija la opción Crear.")
 
 
-def remove_step_title(update, context):
+def remove_step_title(update: Update, context: CallbackContext):
     global title
     title = update.message.text
 
-    if title == '/Cancel':
+    if title == '/Cancel' or title == '/cancel':
         update.message.reply_text(f"Has cancelado la acción!!")
         return ConversationHandler.END
 
     else:
+
         if not isinstance(get_task(title), type(None)):
             remove_task(title)
 
@@ -325,23 +341,52 @@ def remove_step_title(update, context):
                                       f"\n>Si quieres cancelar el proceso, presiona este comando /Cancel")
             return None
 
-        update.message.reply_text(f"La tarea ha sido eliminada con éxito")
+        update.message.reply_text(f"La tarea ha sido eliminada con éxito.\n"
+                                  f"Si desea realizar otra tarea, presione este comando: /Opciones")
         return ConversationHandler.END
 
 
 def alarm(context):
     global chat_id
     ahora = datetime.now()
-    if len(list_tasks) > 0 and list_tasks[0].date_time <= ahora:
-        context.bot.send_message(chat_id = chat_id, text = f"Tarea ha finalizado:\n{str(list_tasks[0])}")
-        print("Fecha primera tarea es menor a la fecha actual. Tarea se elimina de la lista")
-        del list_tasks[0]
+
+    if len(list_tasks) > 0:
+
+        for i in range(len(list_tasks)):
+
+            if list_tasks[i].date_time <= ahora:
+                context.bot.send_message(chat_id = chat_id, text = f"Tarea ha finalizado:\n{str(list_tasks[i])}")
+                del list_tasks[i]
 
 
-def start(update, context):
+def start(update: Update, context: CallbackContext):
     global chat_id
     chat_id = update.message.chat_id
+
+    update.message.reply_text(f"¡¡¡ Bienvenido al bot controlador de tareas !!!\n\n"
+                              f"Para ver las opciones disponibles, solo presiona este comando: /Opciones")
+
     context.job_queue.run_repeating(alarm, interval = 60, context = update.message.chat_id)
+
+
+def options(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s started the conversation.", user.first_name)
+
+    keyboard = [
+            [
+                InlineKeyboardButton("Crear", callback_data = "CREAR"),
+                InlineKeyboardButton("Visualizar", callback_data = "VISUALIZAR"),
+            ],
+            [
+                InlineKeyboardButton("Editar", callback_data = "EDITAR"),
+                InlineKeyboardButton("Eliminar", callback_data = "ELIMINAR"),
+            ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text("Elige una de las siguientes opciones disponibles:", reply_markup = reply_markup)
+    return INIT
 
 
 ############################################
@@ -351,40 +396,30 @@ def main():
 
     dp = updater.dispatcher
 
-    ct_conv_hand = ConversationHandler(
-        entry_points=[CommandHandler('Crear', create_steps)],
-        states={
-            CT_TITLE: [MessageHandler(Filters.text, create_step_title)],
-            CT_DESCRIPTION: [MessageHandler(Filters.text, create_step_description)],
-            CT_DATE: [MessageHandler(Filters.text, create_step_date)]
-        },
-        fallbacks=[CommandHandler('Cancel', create_steps)]
+    conv_handler = ConversationHandler(
+            entry_points = [CommandHandler('Opciones', options)],
+            states = {
+                    INIT: [
+                            CallbackQueryHandler(create_steps, pattern = '^CREAR$'),
+                            CallbackQueryHandler(visualize, pattern = '^VISUALIZAR$'),
+                            CallbackQueryHandler(remove_steps, pattern = '^ELIMINAR$'),
+                            CallbackQueryHandler(edit_steps, pattern = '^EDITAR$')
+                    ],
+                    CREATE_TITLE: [MessageHandler(Filters.text, create_step_title)],
+                    CREATE_DESCRIPTION: [MessageHandler(Filters.text, create_step_description)],
+                    CREATE_DATE: [MessageHandler(Filters.text, create_step_date)],
+                    DELETE: [MessageHandler(Filters.text, remove_step_title)],
+                    EDIT_SEARCH_TITLE: [MessageHandler(Filters.text, edit_step_find)],
+                    EDIT_TITLE: [MessageHandler(Filters.text, edit_step_change_title)],
+                    EDIT_DESCRIPTION: [MessageHandler(Filters.text, edit_step_change_description)],
+                    EDIT_DATE: [MessageHandler(Filters.text, edit_step_change_date)],
+            },
+            fallbacks = [CommandHandler('Opciones', options)],
     )
 
-    de_conv_hand = ConversationHandler(
-        entry_points=[CommandHandler('Eliminar', remove_steps)],
-        states={
-            DE_NAME: [MessageHandler(Filters.text, remove_step_title)],
-        },
-        fallbacks=[CommandHandler('Cancel', remove_steps)]
-    )
-
-    ed_conv_hand = ConversationHandler(
-        entry_points=[CommandHandler('Editar', edit_steps)],
-        states={
-            ED_FIND: [MessageHandler(Filters.text, edit_step_find)],
-            ED_TITLE: [MessageHandler(Filters.text, edit_step_change_title)],
-            ED_DESCRIPTION: [MessageHandler(Filters.text, edit_step_change_description)],
-            ED_DATE: [MessageHandler(Filters.text, edit_step_change_date)]
-        },
-        fallbacks=[CommandHandler('Cancel', edit_steps)]
-    )
-
-    dp.add_handler(CommandHandler('Visualizar', visualize))
     dp.add_handler(CommandHandler('Start', start))
-    dp.add_handler(ct_conv_hand)
-    dp.add_handler(de_conv_hand)
-    dp.add_handler(ed_conv_hand)
+    dp.add_handler(conv_handler)
+
     updater.start_polling()
     updater.idle()
 
